3bdebdc9ac3ec5ae21b59ea2486231e4b102e687
Md. Kamrul Hasan <bsse1418@iit.du.ac.bd>
2025-04-20 15:20:15 +0600

    test

---
+++ lab-01/unittesting/src/test/java/math/ArrayOperationsTest.java
@@ -0,0 +1,39 @@
+package math;
+
+import io.FileIO;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayOperationsTest {
+
+    private final ArrayOperations arrayOperations = new ArrayOperations();
+    private final MyMath myMath = new MyMath();
+    private final FileIO fileio = new FileIO();
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findPrimesInFile1() {
+        int[] actual= arrayOperations.findPrimesInFile(fileio, "src/test/resources/grades_valid.txt", myMath);
+        int[] expected= {2,3};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void findPrimesInFile2() {
+        int[] actual= arrayOperations.findPrimesInFile(fileio, "src/test/resources/primes.txt", myMath);
+        int[] expected= {2,3,5,7,11,13,17,19};
+        assertArrayEquals(expected, actual);
+    }
+
+
+}
